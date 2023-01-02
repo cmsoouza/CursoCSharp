c0815584cbae313c138145c43c29278a948acc21
Cristian <cmsouzaac@outlook.com>
2023-01-01 23:19:28 -0300

    Entrada de Dados

---
+++ Fundamentos/EntradaDeDados.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    internal class EntradaDeDados {
+        public static void Executar() {
+            
+            string frase = Console.ReadLine();
+            
+            Console.WriteLine("Qual sabor de sorvete voçê gosta? " + frase);
+        }
+    }
+}
+
+
+/**
+ * Anotações:
+ * 
+ * - Em definição o comando Readiline() é quando realizamos a entrada de dados
+ *   podemos operar realizando algum tipo de concatenação.
+ * 
+ * 
+ * **/
