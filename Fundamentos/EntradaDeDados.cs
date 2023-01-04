74c0b55f8e199ba520c92a00899440a548435ac0
Cristian <cmsouzaac@outlook.com>
2023-01-04 01:22:09 -0300

    atualização entrada de dados

--- Fundamentos/EntradaDeDados.cs
+++ Fundamentos/EntradaDeDados.cs
@@ -9,9 +9,9 @@ namespace CursoCSharp.Fundamentos {
     internal class EntradaDeDados {
         public static void Executar() {
             // 1° Exemplo;
-            //string frase = Console.ReadLine();
+            string frase = Console.ReadLine();
 
-            //Console.WriteLine("Qual sabor de sorvete voçê gosta? " + frase);
+            Console.WriteLine("Qual sabor de sorvete voçê gosta? " + frase);
 
 
             // 2° Exemplo;
@@ -26,6 +26,16 @@ namespace CursoCSharp.Fundamentos {
             Console.WriteLine(n2.ToString("f2", CultureInfo.InvariantCulture)); // para retorno do inteiro com "."
 
             // 3° Exemplo
+            string[] vet = Console.ReadLine().Split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
 
         }
     }
@@ -50,6 +60,12 @@ namespace CursoCSharp.Fundamentos {
  * - Outra função importante é inserirmos a função "ToString" para converter um dado
  *   de um tipo que não é string. Assim utilizamos a formatação para retorna as casas
  *   decimais usando ("F2" = 2 CASAS DECIMAIS e assim por diante ....)
+ *   
+ * - 3° Exemplo teremos que armazenar utilizando o metodo "Split()" para cada vetor
+ *   usado usando o conceito de arrays.
+ *      => Algumas conversoes sao necessárias nesse exemplo;
+ *          ==> l'31 Utilizando parse para converter em string
+ *          ==> l'32 O mesmo conceito para transformar "int" em uma "string"
  * 
  * 
  * **/
